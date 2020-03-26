3509aa5648b39e5854d9bfb6b398fb77b20ca2c9
Peyton-Cysewski <pcysewski12@gmail.com>
2020-03-25 17:34:49 -0700

    Added Beercard functionality. Still need to add display and sales toggles

--- JS/control.js
+++ JS/control.js
@@ -3,15 +3,31 @@
 // *** This page is where functions specific to the control page are colled *** //
 
 var banner = new Banner();
+banner.textDefault();
 
 var currentInventory = new Inventory();
 currentInventory.loadBeerInventory();
-banner.textDefault();
+console.log(currentInventory);
 
 renderTable();
 
+// Function that grabs form input from beerInventoryForm and uses its information to create Beercard instances
+function createBeercard() {
+  var beername = document.getElementById('beerName').value;
+  var beertype = document.getElementById('beerTypeInput').value;
+  var abv = document.getElementById('abv').value;
+  var hopType = document.getElementById('hopType').value;
+  var shortDescription = document.getElementById('shortDesc').value;
+  var longDescription = document.getElementById('longDesc').value;
 
-// Discretely Defined Event Handler - *** had issues using event handlers in conjunction with the Banner object protoype ***
+  var display = true;
+  var sales = false;
+  var beercard = new Beercard(beername, beertype, abv, hopType, shortDescription, longDescription, display, sales);
+  currentInventory.beers.push(beercard);
+}
+
+
+// Announcement Handler + Listener
 function announcementHandler(event) {
   event.preventDefault();
   localStorage.setItem('announcement', JSON.stringify(document.getElementById('announcement').value));
@@ -20,6 +36,7 @@ function announcementHandler(event) {
 
 document.getElementById('announcementBannerForm').addEventListener('submit', announcementHandler);
 
+// Password Handler + Listener
 function passwordHandler(event) {
   event.preventDefault();
   localStorage.setItem('password', JSON.stringify(document.getElementById('password').value));
@@ -28,28 +45,29 @@ function passwordHandler(event) {
 
 document.getElementById('passwordResetForm').addEventListener('submit', passwordHandler);
 
+// Beer Form Handler + Listener
 function beerFormHandler(event){
   event.preventDefault();
-  currentInventory.addInventory();
+  createBeercard();
   currentInventory.saveToLocalStorage();
-  currentInventory.loadBeerInventory();
   renderTable();
-  console.log(currentInventory);
-
+  // console.log(currentInventory);
 }
 
 document.getElementById('beerInventoryForm').addEventListener('submit', beerFormHandler);
 
+// Takes Care of Making the Inventory Table
 function renderTable() {
-  currentInventory.loadBeerInventory();
   clearTable();
   showTable();
 }
 
+// Erases all current content in the body of the table
 function clearTable() {
   document.getElementById('body').innerHTML = '';
 }
 
+// Draws Table based on what is in currentInventory on control.js
 function showTable() {
 
   for (var i = 0; i < currentInventory.beers.length; i++) {
@@ -57,14 +75,14 @@ function showTable() {
     var report = document.getElementById('body');
     var toggleButton = document.createElement('input');
     toggleButton.setAttribute('type', 'checkbox');
-    toggleButton.setAttribute('id', ('sale' + i ))
+    toggleButton.setAttribute('name', i );
     var toggleButton2 = document.createElement('input');
     toggleButton2.setAttribute('type', 'checkbox');
-    toggleButton2.setAttribute('id', ('display' + i))
+    toggleButton2.setAttribute('name', i);
     var deleteButton = document.createElement('button');
     deleteButton.innerText = 'Remove Item';
-    deleteButton.setAttribute('class', 'removeButton');
-    deleteButton.setAttribute('id', ('delete' + i));
+    deleteButton.setAttribute('type', 'button');
+    deleteButton.setAttribute('name', i);
 
     var tableRow = document.createElement('tr');
     var nameCell = document.createElement('td');
@@ -84,15 +102,23 @@ function showTable() {
 
     report.appendChild(tableRow);
   }
-  console.log(report);
 }
 
+// Handler for removing selected item from Beer Inventory + Listener
 function removeItemFromCart(event) {
-
-  currentInventory.removeItem(event.target.id);
-  // console.log(event.target.id);
-  currentInventory.saveToLocalStorage();
-  renderTable();
+  if (event.target.type === 'button') {
+    currentInventory.removeItem(event.target.name);
+    currentInventory.saveToLocalStorage();
+    renderTable();
+  }
 }
 
-document.getElementById('body').addEventListener('click', removeItemFromCart());
+document.getElementById('body').addEventListener('click', removeItemFromCart);
+
+// Handler for Toggling Beercard Display
+function displayToggle(event) {
+  console.log(event);
+  // if (event.target.type === checkbox)
+};
+
+document.getElementById('body').addEventListener('click', displayToggle);
