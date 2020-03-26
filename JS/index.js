8a4900a9dc9459692f60cc3e53fdef4003afa80a
Peyton-Cysewski <pcysewski12@gmail.com>
2020-03-26 16:24:12 -0700

    Added filter bar!!! There will be a merge conflict regarding style.css

--- JS/index.js
+++ JS/index.js
@@ -70,3 +70,99 @@ function navHandler(event) {
 
 document.getElementById('nav').addEventListener('click', navHandler);
 
+// Filter Toggle Event Handelr and Listener
+// Displays all if none are clicked, if any are clicked, then those are displayed
+function filterHandler(event) {
+  // console.log(event);
+  // Only does things if a checkbox is clicked
+  if (event.target.type === 'checkbox') {
+
+    // Checks if all of the checkboxes are currently unchecked
+    if (
+      document.getElementsByClassName('beerFilter')[0].checked === false &&
+      document.getElementsByClassName('beerFilter')[1].checked === false &&
+      document.getElementsByClassName('beerFilter')[2].checked === false &&
+      document.getElementsByClassName('beerFilter')[3].checked === false &&
+      document.getElementsByClassName('beerFilter')[4].checked === false &&
+      document.getElementsByClassName('beerFilter')[5].checked === false
+    ) {
+      for (var i = 0; i < currentInventory.beers.length; i++) {
+        currentInventory.beers[i].display = true;
+      }
+    }
+
+    // Checks if any of the checkboxes are checked
+    if (
+      document.getElementsByClassName('beerFilter')[0].checked === true ||
+      document.getElementsByClassName('beerFilter')[1].checked === true ||
+      document.getElementsByClassName('beerFilter')[2].checked === true ||
+      document.getElementsByClassName('beerFilter')[3].checked === true ||
+      document.getElementsByClassName('beerFilter')[4].checked === true ||
+      document.getElementsByClassName('beerFilter')[5].checked === true
+    ) {
+
+      // makes all beercards hidden
+      for (var i = 0; i < currentInventory.beers.length; i++) {
+        currentInventory.beers[i].display = false;
+      }
+
+      // If any beercards have a beertype of IPA, then their display property will be set to true
+      if (document.getElementsByClassName('beerFilter')[0].checked === true) {
+        for (var i = 0; i < currentInventory.beers.length; i++) {
+          if (currentInventory.beers[i].beertype === 'IPA' ) {
+            currentInventory.beers[i].display = true;
+          }
+        }
+      }
+
+      // If any beercards have a beertype of Pale Ale, then their display property will be set to true
+      if (document.getElementsByClassName('beerFilter')[1].checked === true) {
+        console.log('test2');
+        for (var i = 0; i < currentInventory.beers.length; i++) {
+          if (currentInventory.beers[i].beertype === 'Pale Ale' ) {
+            currentInventory.beers[i].display = true;
+          }
+        }
+      }
+
+      // If any beercards have a beertype of Porter, then their display property will be set to true
+      if (document.getElementsByClassName('beerFilter')[2].checked === true) {
+        for (var i = 0; i < currentInventory.beers.length; i++) {
+          if (currentInventory.beers[i].beertype === 'Porter' ) {
+            currentInventory.beers[i].display = true;
+          }
+        }
+      }
+
+      // If any beercards have a beertype of Stout, then their display property will be set to true
+      if (document.getElementsByClassName('beerFilter')[3].checked === true) {
+        for (var i = 0; i < currentInventory.beers.length; i++) {
+          if (currentInventory.beers[i].beertype === 'Stout' ) {
+            currentInventory.beers[i].display = true;
+          }
+        }
+      }
+
+      // If any beercards have a beertype of Hefeweizen, then their display property will be set to true
+      if (document.getElementsByClassName('beerFilter')[4].checked === true) {
+        for (var i = 0; i < currentInventory.beers.length; i++) {
+          if (currentInventory.beers[i].beertype === 'Hefeweizen' ) {
+            currentInventory.beers[i].display = true;
+          }
+        }
+      }
+
+      // If any beercards have a beertype of Domestic, then their display property will be set to true
+      if (document.getElementsByClassName('beerFilter')[5].checked === true) {
+        for (var i = 0; i < currentInventory.beers.length; i++) {
+          if (currentInventory.beers[i].beertype === 'Domestic' ) {
+            currentInventory.beers[i].display = true;
+          }
+        }
+      }
+    }
+    currentInventory.writeCards();
+  }
+}
+
+document.getElementById('toggleSwitches').addEventListener('click', filterHandler);
